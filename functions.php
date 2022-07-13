84b1cccfd78ba130ad0a6ffa4ad56cd15d60fd19
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-07-13 14:25:23 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,27 @@ $this->events()->create([
     'category_b' => optional($event)->categoryB,
     'user' => optional($event->user)->accountId,
 ]);
+
+
+$name = DB::table('users')->where('name', 'John')->pluck('name');
+
+var_dump(DB::table('users')->where('id', 1)->pluck('id'));
+
+$user = User::find(1);
+$user->roles()->attach(1);
+
+$user->articles()->syncWithoutDetaching([1]);
+$user->articles()->syncWithoutDetaching([2]);
+$user->articles()->syncWithoutDetaching([1]);
+
+
+$user->articles()->sync([1]);
+$user->articles()->sync([2, 3]);
+$user->articles()->sync([3, 4]);
+
+
+$user->roles()->sync(array(1 => array('expires' => true)));
+
+
+$food = Food::find(1);
+$food->allergies()->sync([1 => ['severity' => 3], 4 => ['severity' => 1]]);
