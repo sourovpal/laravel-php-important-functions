3754bf4f0e2f579f79ed56e621a75397c8428853
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-09-09 08:00:14 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,6 @@
+
+<?php
+
 $this->events()->create([
     'category_a' => optional($event)->categoryA,
     'category_b' => optional($event)->categoryB,
@@ -79,6 +82,42 @@ class User extends Authenticatable
 
 return $this->hasMany('App\Models\Post', 'post_author', 'ID')->where('post_status', 'publish')->latest('App\Models\Post'::CREATED_AT);
 
+
+
+public function setStartDateAttribute($value)
+{
+    $this->attributes['start_date'] = Carbon::createFromFormat(env('Date_Format'), $value)->format('Y-m-d');
+}
+
+public function getStartDateAttribute($value)
+{
+    return Carbon::parse($value)->format(env('Date_Format'));
+}
+
+
+Announcement::with('company', 'department')->latest();
+
+->pluck('id');
+
+Attendance::where('attendance_date', now()->format('Y-m-d'))->distinct('employee_id')->count();
+
+
+DB::beginTransaction();
+try{
+    User::whereId($id)->update($user);
+
+    employee::whereId($id)->update($data);
+
+    DB::commit();
+} catch (Exception $e){
+    DB::rollback();
+    return response()->json(['error' =>  $e->getMessage()]);
+} catch (Throwable $e){
+    DB::rollback();
+    return response()->json(['error' => $e->getMessage()]);
+}
+
+return response()->json(['success' => __('Data Added successfully.')]);
 
 
 
