ffe628ace2b0e882a3b7a0669cb6e9e3e7b5187e
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-09-15 14:29:57 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -122,4 +122,70 @@ return response()->json(['success' => __('Data Added successfully.')]);
 
 
 
+<?php
 
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
+
+
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        return new UserResource($user);
+    }
+}
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::all();
+
+        return UserResource::collection($user);
+    }
+}
+
+
+
+your AppServiceProvider of your Laravel application.
+/**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        JsonResource::withoutWrapping();
+    }
+
+
+public function index(){
+        return PostResource::collection(Post::query()->paginate(3));
+    }
